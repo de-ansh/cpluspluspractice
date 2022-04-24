cafed4bc7bdc16fad8a10c7da8e3f2f0602f3088
Ashish Mishra <anshm9448@gmail.com>
2022-04-25 02:52:54 +0530

    new commits

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -11,18 +11,46 @@ public:
 	}
 	
 };
+node *takeinput(){
+	int data;
+	cin>>data;
+	node *head=NULL;
+	while(data!=0){
+		node *newnode=new node(data);
+		if (head==NULL)
+		{
+			head=newnode;
+		}
+		else{
+			node *temp=head;
+			while(temp->next!=NULL){
+				temp=temp->next;
+			}
+			temp->next=newnode;
+		}
+
+		cin>>data;
+
+	}
+	return head;
+}
+void  print(node *temp){
+	while(temp!=NULL){
+		cout<<temp->data<<"->";
+		temp=temp->next;
+	}
+}
+
 int main(int argc, char const *argv[])
 {
-	node n1(1);
+	/*node n1(1);
 	node *head=&n1;
 	node *n3=new node(10);
 	node *n4=new node(20);
 	head->next=n3;
-	n3->next=n4;
-	node *temp=head;
-	while(temp!=NULL){
-		cout<<temp->data<<"->";
-		temp=temp->next;
-	}	
+	n3->next=n4;*/
+
+	node *temp=takeinput();
+	print(temp);
 	return 0;
 }
