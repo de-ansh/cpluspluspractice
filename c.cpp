88604c4bdb798f2e5758c4046aed1ee773160ef5
Ashish Mishra <anshm9448@gmail.com>
2022-04-26 22:22:14 +0530

    interview

---
+++ c.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Binarysearch(int arr[], int n, int k){
+	int low=0, high=n-1;
+	while(low<=high){
+		int mid= (low+high)/2;
+		if(arr[mid]==k)
+			return mid;
+		if(arr[mid]<k)
+			high=mid-1;
+		else 
+			low= mid+1;
+	}
+	
+}
+int main(int argc, char const *argv[])
+{
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	int k;
+	cin>>k;
+	cout<<Binarysearch(arr,n,k);
+	return 0;
+}
