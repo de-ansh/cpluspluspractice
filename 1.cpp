09dbbe34af02d98e6c1d7e6bc96027aaa86ec97a
Ashish Mishra <anshm9448@gmail.com>
2022-05-10 22:18:35 +0530

    Added few files and edited few files

--- 1.cpp
+++ 1.cpp
@@ -1,45 +1,21 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-
-int longestSubsequence(int arr[], int n){
-	unordered_set<int> S;
-    int ans = 0;
-
-    // Hash all the array elements
-    for (int i = 0; i < n; i++)
-        S.insert(arr[i]);
-
-    // check each possible sequence from
-    // the start then update optimal length
-    for (int i = 0; i < n; i++)
-    {
-        // if current element is the starting
-        // element of a sequence
-        if (S.find(arr[i] - 1) == S.end())
-        {
-            // Then check for next elements
-            // in the sequence
-            int j = arr[i];
-            while (S.find(j) != S.end())
-                j++;
-
-            // update  optimal length if
-            // this length is more
-            ans = max(ans, j - arr[i]);
-        }
+int findDuplicate(int arr[], int n){
+    sort(arr,arr+n);
+    for(int i=0;i<n;i++){
+        if(arr[i]==arr[i-1])
+            return arr[i];
     }
-    return ans;
-
+    return -1;
 }
-int main(int argc, char const *argv[])
-{
-	int n;
-	cin>>n;
-	int arr[n];
-	for (int i = 0; i < n; ++i)
-	{
-		cin>>n;
-	}
-	cout<<longestSubsequence(arr, n);
-	return 0;
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<findDuplicate(arr,n);
+    return 0;
+
 }
--- BST.cpp
+++ BST.cpp
@@ -44,6 +44,25 @@ Node *build(){
  	cout<<root->data<<" ";
  	inorder(root->right);
  }
+ void bfs(Node *root){
+ 	vector<vector<int>> ans;
+ 	if(root== NULL) return;
+ 	queue<Node *>q;
+ 	q.push(root);
+ 	while(!q.empty()){
+ 		int size=q.size();
+ 		vector<int>level;
+ 		for(int i=0;i<size;i++){
+ 			Node *node=q.front();
+ 			q.pop();
+ 			if(node->left!=NULL) q.push(node->left);
+ 			if(node->right!=NULL) q.push(node->right);
+ 			level.push_back(node->val);
+ 		}
+ 		ans.push_back(level);
+ 	}
+ 	return ans;
+ }
  int main(int argc, char const *argv[])
  {
  	Node *root=build();
