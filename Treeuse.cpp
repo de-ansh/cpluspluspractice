b9f11a4f2762a60ec5dfe182c292e677ad431a02
Ashish Mishra <anshm9448@gmail.com>
2022-05-23 00:09:29 +0530

    working on trees and giving user input

--- Treeuse.cpp
+++ Treeuse.cpp
@@ -1,19 +1,44 @@
 #include"TreeNode.h"
 #include<bits/stdc++.h>
 using namespace std;
+TreeNode <int> *takeinput(){
+	int rootData;
+	cout<<"Enter Data"<<endl;
+	cin>>rootData;
+	TreeNode<int>* root=new TreeNode<int> (rootData);
+	int n;
+	cout<<"enter the number of child of "<<rootData<<endl;
+	cin>>n;
+	for (int i = 0; i < n; ++i)
+	{
+		TreeNode<int>*child=takeinput();
+		root->children.push_back(child);
+	}
+	return root;
+}
 void printTree(TreeNode<int>*root){
-	cout<<root->data<<endl;
+	if (root==NULL)
+	{
+		return;
+	}
+	cout<<root->data<<": ";
+	for (int i = 0; i < root->children.size(); ++i)
+	{
+		cout<<root->children[i]->data<<",";
+	}
+	cout<<endl;
 	for (int i = 0; i < root->children.size(); ++i)
 	{
 		printTree(root->children[i]);
 	}
 }
 int main(){
-	TreeNode<int> *root= new TreeNode<int>(1);
+	/*TreeNode<int> *root= new TreeNode<int>(1);
 	TreeNode<int> *node1= new TreeNode<int>(2);
 	TreeNode<int> *node2= new TreeNode<int>(45);
 	root->children.push_back(node1);
-	root->children.push_back(node2);
+	root->children.push_back(node2);*/
+	TreeNode<int>*root=takeinput();
 	printTree(root);
 
 }
