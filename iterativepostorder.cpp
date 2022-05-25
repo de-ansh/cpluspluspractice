6e58011114d73760296e3226f88ef160ddb2ddcf
Ashish Mishra <anshm9448@gmail.com>
2022-05-25 16:22:53 +0530

    Working on Trees

--- iterativepostorder.cpp
+++ iterativepostorder.cpp
@@ -7,7 +7,7 @@ struct node {
 
 vector < int > postOrderTrav(node * curr) {
 
-  vector < int > postOrder;
+  /*vector < int > postOrder;
   if (curr == NULL) return postOrder;
 
   stack < node * > s1;
@@ -25,6 +25,30 @@ vector < int > postOrderTrav(node * curr) {
   while (!s2.empty()) {
     postOrder.push_back(s2.top() -> data);
     s2.pop();
+  }
+  return postOrder;*/
+  vector < int > postOrder;
+  if (cur == NULL) return postOrder;
+
+  stack < node * > st;
+  while (cur != NULL || !st.empty()) {
+
+    if (cur != NULL) {
+      st.push(cur);
+      cur = cur -> left;
+    } else {
+      node * temp = st.top() -> right;
+      if (temp == NULL) {
+        temp = st.top();
+        st.pop();
+        postOrder.push_back(temp -> data);
+        while (!st.empty() && temp == st.top() -> right) {
+          temp = st.top();
+          st.pop();
+          postOrder.push_back(temp -> data);
+        }
+      } else cur = temp;
+    }
   }
   return postOrder;
 }
